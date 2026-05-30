d693a4751702551977ea8791161af453c7ddea13
Thi Nhúng <khanhquan2k@gmail.com>
2026-05-30 15:23:30 +0700

    Ignore server files

--- server/CHAY_AN_GIAO_DIEN.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Chạy ẩn terminal Python, chỉ hiện giao diện Tkinter. Cloudflared tunnel cũng chạy ẩn từ trong may_chu_lua.py.
-
-cd /d D:\DuAn\DoAn\server
-start "" "C:\Users\Administrator\AppData\Local\Programs\Python\Python310\pythonw.exe" may_chu_lua.py
--- server/khoi_dong_may_chu.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-REM Chạy trực tiếp file may_chu_lua.py. File Python sẽ tự mở Cloudflared tunnel.
-
-cd /d D:\DuAn\DoAn\server
-"C:\Users\Administrator\AppData\Local\Programs\Python\Python310\python.exe" may_chu_lua.py
