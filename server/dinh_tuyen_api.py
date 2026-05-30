2eba942c427fae9d8b762557bfbd5b102b0b007c
Thi Nhúng <khanhquan2k@gmail.com>
2026-05-30 15:38:12 +0700

    #test gửi lên server

--- server/dinh_tuyen_api.py
+++ server/dinh_tuyen_api.py
@@ -20,7 +20,7 @@ from xu_ly_khu import CapNhatCaiDatKhu, LayAnhChupTrangThaiKhu, ThemKhu, XoaKhu
 from xu_ly_lich_su import DocLichSuKhu, GhiLichSuKhu, THU_MUC_MINH_CHUNG, XoaLichSuKhu
 from xu_ly_thong_bao_day import DangKyTokenDiDong, LayDanhSachTokenDiDong
 #chán quán
-
+#test
 def DangKyRoutes(app):
     """Đăng ký toàn bộ routes cho ứng dụng Flask."""
 
